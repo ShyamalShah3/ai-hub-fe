80203c76d39b1f7807772627ad59a078a28b30dc
ShyamalShah3 <shyamalgs@gmail.com>
2024-10-02 23:44:04 -0700

    Create Homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom'
 import './homePage.css'
+import { IconButton } from '@mui/material'
+import Fingerprint from '@mui/icons-material/Fingerprint'
 
 const HomePage = () => {
     return (
-        <div className='homepage'>
-            <Link to="/dashboard">Dashboard</Link>
+        <div className="homepage">
+            <div className="left">
+                <h1>AI Hub</h1>
+                <Link to="/dashboard" style={{ textDecoration: 'none' }}>
+                    <IconButton aria-label='fingerprint' color='secondary' size='large' className='fingerprint-button'>
+                        <Fingerprint />
+                    </IconButton>
+                </Link>
+            </div>
         </div>
     )
 }
