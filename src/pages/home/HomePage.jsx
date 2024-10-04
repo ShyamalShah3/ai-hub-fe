9e5b0489c00781d9c744b425c959c02a0bab03e8
ShyamalShah3 <shyamalgs@gmail.com>
2024-10-03 21:50:01 -0700

    Add Typing Animation on Homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,18 +2,37 @@ import { Link } from 'react-router-dom'
 import './homePage.css'
 import { IconButton } from '@mui/material'
 import Fingerprint from '@mui/icons-material/Fingerprint'
+import { TypeAnimation } from 'react-type-animation'
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="left">
-                <h1>AI Hub</h1>
-                <Link to="/dashboard" style={{ textDecoration: 'none' }}>
-                    <IconButton aria-label='fingerprint' color='secondary' size='large' className='fingerprint-button'>
-                        <Fingerprint />
-                    </IconButton>
-                </Link>
-            </div>
+            <h1>
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'AI Hub',
+                        1000,
+                        'AI Agents',
+                        1000,
+                        'AI Tools',
+                        1000,
+                        'AI Services',
+                        1000,
+                        'AI Workflows',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{ fontSize: '1em', display: 'inline-block' }}
+                    repeat={Infinity}
+                />
+            </h1>
+            <Link to="/dashboard" style={{ textDecoration: 'none' }}>
+                <IconButton aria-label='fingerprint' color='secondary' size='large' className='fingerprint-button'>
+                    <Fingerprint />
+                </IconButton>
+            </Link>
         </div>
     )
 }
